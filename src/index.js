b9b4ead5fae3bd9508df3b4570dcdeb4f866fe35
Jonathan de Leon <jdeleona12@miumg.edu.gt>
2026-06-03 10:56:12 -0600

    cambio en index para coneccion a Vercel

--- src/index.js
+++ src/index.js
@@ -5,7 +5,13 @@ require('dotenv').config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: [
+    'http://localhost:5173',
+    'https://TU-APP.vercel.app', // lo actualizas después con tu URL real de Vercel
+  ],
+  credentials: true,
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/uploads', express.static(path.join(__dirname, '../uploads')));
